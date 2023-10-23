ad7c990b8ff1b8a6b22ce1759038a29e8286ae8b
Gideon Kim <gideon.kim@gordon.edu>
2023-10-23 09:29:38 -0400

    add new schema migration and modify server.js to add data to database

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,8 +1,18 @@
-import { json } from '@sveltejs/kit';
+import prisma from '$lib/prisma';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-    const { json } = await request.json();
-    console.log(json)
+    const data = await request.json();
+    await prisma.app.upsert({
+        where: {
+            appId: data.appId
+        },
+        create: data,
+        update: {
+            appIcon: data.appIcon,
+            googlePlayUrl: data.googlePlayUrl,
+            listings: data.listings
+        }
+    });
     return new Response(null, { status: 201 });
 }
