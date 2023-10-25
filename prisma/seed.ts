99b04caaf98c42733924a35278da9a8da75368da
Chris Hubbard <chris@thehubbards.org>
2023-10-25 16:07:41 -0400

    Add Prettier rules and linting GitHub Action (#9)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,12 @@
 // prisma/seed.ts
 
-import { PrismaClient } from '@prisma/client'
-import appData from "./data.json" assert { type: "json" }
+import { PrismaClient } from '@prisma/client';
+import appData from './data.json' assert { type: 'json' };
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-    console.log(`Start seeding ...`)
+    console.log(`Start seeding ...`);
 
     for (const a of appData) {
         const app = await prisma.app.create({
@@ -20,29 +20,29 @@ async function main() {
                             lang: a.listings[0].lang,
                             title: a.listings[0].title,
                             shortDescription: a.listings[0].shortDescription,
-                            fullDescription: a.listings[0].fullDescription,
+                            fullDescription: a.listings[0].fullDescription
                         },
                         {
                             lang: a.listings[1].lang,
                             title: a.listings[1].title,
                             shortDescription: a.listings[1].shortDescription,
-                            fullDescription: a.listings[1].fullDescription,
+                            fullDescription: a.listings[1].fullDescription
                         }
-                    ],
-                },
+                    ]
+                }
             }
-        })
-        console.log(`Created user with id: ${app.id}`)
+        });
+        console.log(`Created user with id: ${app.id}`);
     }
-    console.log(`Seeding finished.`)
+    console.log(`Seeding finished.`);
 }
 
 main()
     .then(async () => {
-        await prisma.$disconnect()
+        await prisma.$disconnect();
     })
     .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
--- src/routes/app/[id]/+page.svelte
+++ src/routes/app/[id]/+page.svelte
@@ -2,20 +2,20 @@
 
 <!-- 1. -->
 <script lang="ts">
-	//	import Post from '$lib/components/Post.svelte';
-	import type { PageData } from './$types';
+    //	import Post from '$lib/components/Post.svelte';
+    import type { PageData } from './$types';
 
-	export let data: PageData;
+    export let data: PageData;
 </script>
 
 <div>
-	<h1>{data.app?.listings[0].title}</h1>
-	<main>
-		<p>
-			{data.app?.listings[0].shortDescription}
-		</p>
-		<p>
-			{data.app?.listings[0].fullDescription}
-		</p>
-	</main>
+    <h1>{data.app?.listings[0].title}</h1>
+    <main>
+        <p>
+            {data.app?.listings[0].shortDescription}
+        </p>
+        <p>
+            {data.app?.listings[0].fullDescription}
+        </p>
+    </main>
 </div>
