49a0f232decf74a8af4fd7994756cee79dff63c3
Sharon Patta <sharonpatta1@MacBook-Pro-5.gordon.edu>
2023-11-01 15:28:26 -0400

    local smtp setup

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,4 +1,4 @@
-const nodemailer = require("nodemailer");
+import nodemailer from 'nodemailer';
 
 /* async function sendVerificationEmail(receiverEmail, verificationLink) {
     // create a transporter object using the default SMTP transport
@@ -24,20 +24,15 @@ const nodemailer = require("nodemailer");
 
   sendVerificationEmail("receiver@example.com", "https://www.google.com/?client=safari&channel=mac_bm"); */
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
+  const transporter = nodemailer.createTransport({
+    host: 'localhost',
+    port: 1025,
     auth: {
-      user: 'usercontenttest@gmail.com',
-      pass: 'Thisisatest123!'
+        user: 'project.1',
+        pass: 'secret.1'
     }
 });
 
-let message = transporter.sendMail({
-    from: 'usercontenttest@gmail.com', // sender address
-    to: 'jaeminlee2001@gmail.com', // list of receivers
-    subject: "Email Verification", // Subject line
-    text: "Click the link below to verify your email:", // plain text body
-  });
 
-transporter.sendMail(message);
+export default transporter;
   
--- src/routes/app/[id]/+page.server.ts
+++ src/routes/app/[id]/+page.server.ts
@@ -1,12 +1,20 @@
 // src/routes/p/[id]/+page.server.ts
 import prisma from '$lib/prisma';
 import type { PageServerLoad } from './$types';
+import transporter from '$lib/mailer';
 
 export const load = (async ({ params: { id } }) => {
     const app = await prisma.app.findUnique({
         where: { appId: String(id) },
         include: { listings: true }
     });
+
+    transporter.sendMail({
+        from: 'usercontenttest@gmail.com', // sender address
+        to: 'jaeminlee2001@gmail.com', // list of receivers
+        subject: "Email Verification", // Subject line
+        text: "Click the link below to verify your email:", // plain text body
+      });
 
     return { app };
 }) satisfies PageServerLoad;
