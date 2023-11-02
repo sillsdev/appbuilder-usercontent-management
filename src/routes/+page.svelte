34fe5525402233a7a3023fda1948840a57a8232b
Sharon Patta <sharonpatta1@MacBook-Pro-5.gordon.edu>
2023-11-01 23:02:50 -0400

    formatted html for email (pair programmed with Jay and Gideon)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to the User Content Management Page!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Email Confirmation</title>
+    </head>
+    <body style="font-family: Arial, sans-serif; text-align: center;">
+        <div style="max-width: 600px; margin: auto; padding: 20px;">
+            <h1 style="font-size: 48px; color: #0077b6;">Verification</h1>
+
+            <p style="font-size: 24px;">Hello [email address],</p>
+
+            <p style="line-height: 2;">
+                Thanks for typing in your email!
+
+                <br /> Click the link below to confirm your email address:
+            </p>
+
+            <a
+                href="#"
+                style="background-color: #0077b6; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 20px 0; cursor: pointer; border-radius: 4px;"
+            >
+                Verify Now
+            </a>
+
+            <p>This verification link will expire in 24 hours. -From Team SIL</p>
+        </div>
+    </body>
+</html>
--- src/routes/app/[id]/+page.server.ts
+++ src/routes/app/[id]/+page.server.ts
@@ -14,9 +14,39 @@ export const load = (async ({ params: { id } }) => {
 
     transporter.sendMail({
         from: PUBLIC_EMAIL_FROM, // sender address
-        to: 'jaeminlee2001@gmail.com', // list of receivers
+        to: 'sharonepatta@gmail.com', // list of receivers
         subject: 'Email Verification', // Subject line
-        text: 'Click the link below to verify your email:' // plain text body
+        text: 'Click the link below to verify your email:', // plain text body
+        amp: `<!DOCTYPE html>
+        <html>
+        <head>
+            <title>Email Confirmation</title>
+        </head>
+        <body style="font-family: Arial, sans-serif; text-align: center;">
+        
+            <div style="max-width: 600px; margin: auto; padding: 20px;">
+                <h1 style="font-size: 48px; color: #0077b6;">Verification</h1>
+        
+                <p style="font-size: 24px;">Hello [email address],</p>
+        
+                <p style="line-height: 2;">
+                    Thanks for typing in your email! 
+        
+                 <br /> Click the link below to confirm your email address:
+                </p>
+        
+                <a href="#" style="background-color: #0077b6; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 20px 0; cursor: pointer; border-radius: 4px;">
+                    Verify Now
+                </a>
+        
+        
+                <p>
+                    This verification link will expire in 24 hours. -From Team SIL
+                </p>
+            </div>
+        
+        </body>
+        </html>`
     });
 
     return { app };
