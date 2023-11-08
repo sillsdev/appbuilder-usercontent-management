fe793c0cf5f7846f785c68b274eebb129701d3f3
Sharon Patta <sharonpatta1@MacBook-Pro-5.gordon.edu>
2023-11-08 16:13:02 -0500

    revert mailer.ts

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,21 +1,20 @@
 import nodemailer from 'nodemailer';
 import type SMTPTransport from 'nodemailer/lib/smtp-transport';
-/*import {
+import {
     SMTP_SERVER_ADDRESS,
     SMTP_SERVER_PORT,
     SMTP_SERVER_USERNAME,
     SMTP_SERVER_PASSWORD
-} from '$env/static/private';*/
+} from '$env/static/private';
 
 const smtpConfig: SMTPTransport.Options = {
-  host: process.env.SMTP_SERVER_ADDRESS,
-  port: Number(process.env.SMTP_SERVER_PORT),
-  auth: {
-      user: process.env.SMTP_SERVER_USERNAME,
-      pass: process.env.SMTP_SERVER_PASSWORD
-  }
+    host: SMTP_SERVER_ADDRESS,
+    port: Number(SMTP_SERVER_PORT),
+    auth: {
+        user: SMTP_SERVER_USERNAME,
+        pass: SMTP_SERVER_PASSWORD
+    }
 };
-
 const transporter = nodemailer.createTransport(smtpConfig);
 
 export default transporter;
