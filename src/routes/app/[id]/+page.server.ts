b07d8f8111979f5c92be5d03c6713b32619d540e
sharonpatta <52753413+sharonpatta@users.noreply.github.com>
2023-11-09 11:43:42 -0500

    progress on nodemailer setup - credit to chatGPT for some setup config (#11)
    * progress on nodemailer setup - credit to chatGPT for some setup config
    
    * local smtp setup
    
    * Suggest changes for nodemailer to work through environment
    
    * formatted html for email (pair programmed with Jay and Gideon)
    
    * Fix issues with PR#11
    
    * change env vars from EMAIL_ to SMTP_ to match their use
    * change env var EMAIL_FROM to be private
    * use nodemailer.SMTPTransport to fix error in mailer.ts
    * include lint.env to fix errors with import of env during lint
    * use lint.env in lint workflow
    
    * fix issue with html rendering
    
    * reset home page
    
    * remove unneeded code in mailer.ts
    
    * update lint.env
    
    * update .env.example
    
    * export env vars in pipeline
    
    * update env var access (credit to Jay and chatgpt)
    
    * revert mailer.ts
    
    * readd exports to test.yml
    
    * reformat indentation
    
    * revert page.svelte for integration tests
    
    * remove credentials
    
    * readd exports with filler values
    
    * Use example.env in lint and test
    
    ---------
    
    Co-authored-by: Sharon Patta <sharonpatta1@MacBook-Pro-5.gordon.edu>
    Co-authored-by: Chris Hubbard <chris@thehubbards.org>

--- src/routes/app/[id]/+page.server.ts
+++ src/routes/app/[id]/+page.server.ts
@@ -1,11 +1,52 @@
 // src/routes/p/[id]/+page.server.ts
 import prisma from '$lib/prisma';
 import type { PageServerLoad } from './$types';
+import transporter from '$lib/mailer';
+import { EMAIL_FROM } from '$env/static/private';
 
 export const load = (async ({ params: { id } }) => {
     const app = await prisma.app.findUnique({
         where: { appId: String(id) },
         include: { listings: true }
+    });
+
+    console.log('Emailing to Jae to verify link');
+
+    transporter.sendMail({
+        from: EMAIL_FROM, // sender address
+        to: ['usercontenttest@gmail.com'], // list of receivers
+        subject: 'Email Verification', // Subject line
+        text: 'Click the link below to verify your email:', // plain text body
+        html: `<!DOCTYPE html>
+        <html>
+        <head>
+            <title>Email Confirmation</title>
+        </head>
+        <body style="font-family: Arial, sans-serif; text-align: center;">
+        
+            <div style="max-width: 600px; margin: auto; padding: 20px;">
+                <h1 style="font-size: 48px; color: #0077b6;">Verification</h1>
+        
+                <p style="font-size: 24px;">Hello [email address],</p>
+        
+                <p style="line-height: 2;">
+                    Thanks for typing in your email! 
+        
+                 <br /> Click the link below to confirm your email address:
+                </p>
+        
+                <a href="#" style="background-color: #0077b6; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 20px 0; cursor: pointer; border-radius: 4px;">
+                    Verify Now
+                </a>
+        
+        
+                <p>
+                    This verification link will expire in 24 hours. -From Team SIL
+                </p>
+            </div>
+        
+        </body>
+        </html>`
     });
 
     return { app };
