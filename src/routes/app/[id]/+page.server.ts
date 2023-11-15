c03a0990999276ace20439ee18040eb01f19b99c
Gideon Kim <78437239+mla04762@users.noreply.github.com>
2023-11-15 15:26:30 -0500

    Add submittable textbox for email address (#14)
    * Add submitable textbox for email address
    
    * Add any type for request
    
    * Format page.server.ts file
    
    * Modified request type and got rid of sendMail function
    
    * Fix some logic for when email address is null
    
    * Get rid of unused variables
    
    * Change fail code to 500 (internal server error)
    
    * Change fail code to 500 (internal server error)
    
    ---------
    
    Co-authored-by: Sharon Patta <sharonpatta1@MacBook-Pro-5.gordon.edu>

--- src/routes/app/[id]/+page.server.ts
+++ src/routes/app/[id]/+page.server.ts
@@ -1,53 +1,77 @@
 // src/routes/p/[id]/+page.server.ts
 import prisma from '$lib/prisma';
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, RequestEvent } from './$types';
 import transporter from '$lib/mailer';
 import { EMAIL_FROM } from '$env/static/private';
+import type { Options } from 'nodemailer/lib/mailer';
+import { fail } from '@sveltejs/kit';
 
 export const load = (async ({ params: { id } }) => {
     const app = await prisma.app.findUnique({
         where: { appId: String(id) },
         include: { listings: true }
     });
-
-    console.log('Emailing to Jae to verify link');
-
-    transporter.sendMail({
-        from: EMAIL_FROM, // sender address
-        to: ['usercontenttest@gmail.com'], // list of receivers
-        subject: 'Email Verification', // Subject line
-        text: 'Click the link below to verify your email:', // plain text body
-        html: `<!DOCTYPE html>
-        <html>
-        <head>
-            <title>Email Confirmation</title>
-        </head>
-        <body style="font-family: Arial, sans-serif; text-align: center;">
-        
-            <div style="max-width: 600px; margin: auto; padding: 20px;">
-                <h1 style="font-size: 48px; color: #0077b6;">Verification</h1>
-        
-                <p style="font-size: 24px;">Hello [email address],</p>
-        
-                <p style="line-height: 2;">
-                    Thanks for typing in your email! 
-        
-                 <br /> Click the link below to confirm your email address:
-                </p>
-        
-                <a href="#" style="background-color: #0077b6; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 20px 0; cursor: pointer; border-radius: 4px;">
-                    Verify Now
-                </a>
-        
-        
-                <p>
-                    This verification link will expire in 24 hours. -From Team SIL
-                </p>
-            </div>
-        
-        </body>
-        </html>`
-    });
-
     return { app };
 }) satisfies PageServerLoad;
+
+export const actions = {
+    default: async ({ request }: RequestEvent) => {
+        try {
+            const formData = await request.formData();
+            const email = formData.get('to');
+            if (email !== '' && typeof email === 'string') {
+                const message: Options = {
+                    from: EMAIL_FROM,
+                    to: email,
+                    subject: 'Email Verification',
+                    text: 'Click the link below to verify your email:',
+                    html: `<!DOCTYPE html>
+                <html>
+                <head>
+                    <title>Email Confirmation</title>
+                </head>
+                <body style="font-family: Arial, sans-serif; text-align: center;">
+                
+                    <div style="max-width: 600px; margin: auto; padding: 20px;">
+                        <h1 style="font-size: 48px; color: #0077b6;">Verification</h1>
+                
+                        <p style="font-size: 24px;">Hello [email address],</p>
+                
+                        <p style="line-height: 2;">
+                            Thanks for typing in your email! 
+                
+                         <br /> Click the link below to confirm your email address:
+                        </p>
+                
+                        <a href="#" style="background-color: #0077b6; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 20px 0; cursor: pointer; border-radius: 4px;">
+                            Verify Now
+                        </a>
+                
+                
+                        <p>
+                            This verification link will expire in 24 hours. -From Team SIL
+                        </p>
+                    </div>
+                
+                </body>
+                </html>`
+                };
+
+                transporter.sendMail(message, (err) => {
+                    if (err) {
+                        console.error(err);
+                        return { success: false };
+                    }
+                });
+
+                return {
+                    success: 'Email is sent'
+                };
+            } else {
+                return fail(500);
+            }
+        } catch (error) {
+            return fail(500);
+        }
+    }
+};
--- src/routes/app/[id]/+page.svelte
+++ src/routes/app/[id]/+page.svelte
@@ -6,6 +6,7 @@
     import type { PageData } from './$types';
 
     export let data: PageData;
+    export let form;
 </script>
 
 <div>
@@ -18,4 +19,15 @@
             {data.app?.listings[0].fullDescription}
         </p>
     </main>
+    <fieldset>
+        <legend>Verify Email</legend>
+        <form method="POST" class="container">
+            <div class="input">
+                <label for="">Email Address:</label>
+                <input name="to" type="email" value="" />
+            </div>
+            <button type="submit">Send</button>
+        </form>
+        <p class="success">{form?.success || ''}</p>
+    </fieldset>
 </div>
