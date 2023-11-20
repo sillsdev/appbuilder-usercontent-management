60cc74ff1338282ff738aa8f53908a68f1e522b7
Jae Min Lee <jaemin.lee@gordon.edu>
2023-11-19 20:18:28 -0500

    Added prisma model and generated verification code to send in email

---
+++ prisma/migrations/20231116185903_user_management_request/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "UserManagementRequest" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "confirmationCode" TEXT NOT NULL,
+    "dateExpires" TIMESTAMP(3) NOT NULL,
+    "appId" INTEGER NOT NULL,
+    "changeRequest" TEXT NOT NULL,
+    "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "dateCompleted" TIMESTAMP(3) NOT NULL,
+    "dateConfirmed" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserManagementRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "UserManagementRequest" ADD CONSTRAINT "UserManagementRequest_appId_fkey" FOREIGN KEY ("appId") REFERENCES "App"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
