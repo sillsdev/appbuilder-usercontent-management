e0b9c0dfdbf6d7af370fc92d5594dfd4098a3305
Gideon Kim <gideon.kim@gordon.edu>
2024-01-29 10:21:49 -0500

    move metadata component to component folder and add sizing for mobile for initial page

--- src/components/AppMetadata.svelte
+++ src/components/AppMetadata.svelte
@@ -11,7 +11,7 @@
         class="w-32 h-32 object-cover rounded-md shadow-md mx-auto mb-4"
     />
     <h1 class="text-2xl font-bold">{data.app?.listings[0].title}</h1>
-    <main class="max-w-screen-md prose">
+    <main class="max-w-screen-md prose sm:prose-lg md:prose-xl lg:prose-2xl xl:prose-3xl">
         <p class="text-3xl">{data.app?.listings[0].shortDescription}</p>
         <p>{data.app?.listings[0].fullDescription}</p>
     </main>
