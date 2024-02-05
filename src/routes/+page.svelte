802f368b6f3117095819b4ad81bfc468e2c861a0
Gideon Kim <gideon.kim@gordon.edu>
2024-02-04 19:58:54 -0500

    Remove example components from initial page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="btn">asdfasd</button>
-
-<div class="card w-96 bg-base-100 shadow-xl">
-    <figure>
-        <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-        />
-    </figure>
-    <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-        </div>
-    </div>
-</div>
