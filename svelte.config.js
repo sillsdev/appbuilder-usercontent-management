a8885f474a14b00ff04fd62d2f8c2aae08e734ec
Jae Min Lee <jaemin.lee@gordon.edu>
2024-02-08 11:21:30 -0500

    Match main

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,6 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
     preprocess: [
         preprocess({
             postcss: true
@@ -20,4 +18,5 @@ const config = {
         adapter: adapter()
     }
 };
+
 export default config;
