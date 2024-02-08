6174aa28745f14049137e8092c972fdcfc3b8460
Jae Min (Jay) Lee <112501914+jaeminlee0515@users.noreply.github.com>
2024-02-08 13:29:41 -0500

    Change to DaisyUI for verification page (#25)
    * Change to DaisyUI for verification page
    
    * Add more UI improvements to page
    
    * Add more styling based on feedback
    
    * Make UI more mobile-friendly and adjust measurements
    
    * Remove tailwind duplicate
    
    * Match main
    
    ---------
    
    Co-authored-by: Jae Min Lee <jaemin.lee@gordon.edu>

--- src/routes/verify/[id]/+page.svelte
+++ src/routes/verify/[id]/+page.svelte
@@ -4,32 +4,26 @@
     export let data: PageData;
 
     let code = ['', '', '', '', '', ''];
-    // Function to update the code
+
     function updateCode(index: number, event: Event) {
-        if (event.target === null) {
-            return;
-        }
-        const { value } = event.target as HTMLInputElement;
-        // Check if the last character is a digit
-        const lastChar = value.slice(-1);
+        const input = event.target as HTMLInputElement;
+        const lastChar = input.value.slice(-1);
         if (lastChar.match(/^\d$/)) {
-            code[index] = lastChar; // Set only if it's a digit
+            code[index] = lastChar;
             if (index < code.length - 1) {
-                document.getElementById(`input-${index + 1}`)?.focus(); // move to next input
+                document.getElementById(`input-${index + 1}`)?.focus();
             }
         } else {
-            // If not a digit, clear the input
             code[index] = '';
-            (event.target as HTMLInputElement).value = '';
+            input.value = '';
         }
     }
 
-    // Function to handle keydown events for backspace
     function handleBackspace(index: number, event: KeyboardEvent) {
         if (event.key === 'Backspace' && !code[index]) {
             if (index > 0) {
-                document.getElementById(`input-${index - 1}`)?.focus(); // move to previous input
-                code[index - 1] = ''; // clear the previous input
+                document.getElementById(`input-${index - 1}`)?.focus();
+                code[index - 1] = '';
             }
         }
     }
@@ -38,28 +32,36 @@
 </script>
 
 <div class="verification-container">
-    <h2>Verify your email address</h2>
+    <img src="/email-icon.png" alt="Email Verification Icon" class="email-icon" />
+    <h1>Verify your email address</h1>
+    <br />
     <p>
-        We emailed you a six-digit code to {data.request?.email}. Enter the code below to confirm
-        your email address.
+        Thank You! We emailed you a six-digit code to <span class="font-bold text-black"
+            >{data.request?.email}.</span
+        >
+        <br />
+        <span class="text-center block">Enter the code below to confirm your email address. </span>
     </p>
+    <br />
 
     <form method="POST" class="code-form">
-        {#each code as value, index}
-            <input
-                class="code-input"
-                type="text"
-                id={`input-${index}`}
-                maxlength="1"
-                name={`input-${index}`}
-                {value}
-                on:input={(event) => updateCode(index, event)}
-                on:keydown={(event) => handleBackspace(index, event)}
-            />
-        {/each}
-        <button disabled={!isCodeComplete} type="submit">Verify</button>
+        <div class="inputs-row">
+            {#each code as value, index}
+                <input
+                    class="code-input"
+                    type="text"
+                    id={`input-${index}`}
+                    maxlength="1"
+                    name={`input-${index}`}
+                    {value}
+                    on:input={(event) => updateCode(index, event)}
+                    on:keydown={(event) => handleBackspace(index, event)}
+                />
+            {/each}
+        </div>
     </form>
 </div>
+<button class="btn btn-primary" disabled={!isCodeComplete} type="submit"> Verify </button>
 
 <style>
     .verification-container {
@@ -72,16 +74,22 @@
         border-radius: 8px;
         width: fit-content;
         margin: auto;
+        background-color: white;
+    }
+
+    h1 {
+        color: #007bff;
+        font-size: 32px;
     }
 
     .code-input {
         width: 50px;
         height: 50px;
-        margin: 10px;
+        margin: 5px;
         text-align: center;
         font-size: 24px;
         border: 2px solid #d9d9d9;
-        border-radius: 4px;
+        border-radius: 8px;
         transition: border-color 0.3s;
     }
 
@@ -89,16 +97,57 @@
         border-color: #007bff;
         outline: none;
     }
-    .code-form {
+
+    .email-icon {
+        width: 100px;
+        height: auto;
+        margin-bottom: 20px;
+    }
+
+    .inputs-row {
         display: flex;
         justify-content: center;
     }
+
+    .code-form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
     button {
         padding: 0px 30px;
         border: none;
         background-color: #007bff;
         color: white;
-        border-radius: 4px;
+        border-radius: 8px;
         cursor: pointer;
+        transition: background-color 0.3s;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    button:hover {
+        background-color: #0056b3;
+    }
+
+    h1,
+    p {
+        width: 100%;
+        text-align: center;
+        margin: 10px 0;
+    }
+
+    @media (max-width: 768px) {
+        .verification-container {
+            width: 90%;
+            margin-top: 25%;
+            box-sizing: border-box;
+        }
+
+        .code-input {
+            margin: 2px;
+        }
     }
 </style>
