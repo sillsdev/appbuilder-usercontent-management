4c2f762e4824bd45c1827c3ecf193baf58f7d964
Gideon Kim <78437239+mla04762@users.noreply.github.com>
2024-02-08 11:00:14 -0500

    Enhance UI on Initial Page (#24)
    * Initial
    
    * Install DaisyUI
    
    * Run Prettier
    
    * Install DaisyUI
    
    * Make changes to UI to match LOFI UI design
    
    * add component for app metadata
    
    * move metadata component to component folder and add sizing for mobile for initial page
    
    * Change directories
    
    * change directory for appmetadata component
    
    * Add expand button
    
    * remove comments
    
    * Remove unused variables
    
    * Remove example components from initial page
    
    * Use props on AppMetadata component
    
    ---------
    
    Co-authored-by: Chris Hubbard <chris@thehubbards.org>

---
+++ src/lib/components/AppMetadata.svelte
@@ -0,0 +1,35 @@
+<!-- AppDetails.svelte -->
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    export let appIcon: string | undefined;
+    export let title: string | undefined;
+    export let shortDescription: string | undefined;
+    export let fullDescription: string | undefined;
+
+    let isExpanded = false;
+    const dispatch = createEventDispatcher();
+
+    const toggleDescription = () => {
+        isExpanded = !isExpanded;
+        dispatch('toggle', { isExpanded });
+    };
+</script>
+
+<div>
+    <img
+        src={appIcon}
+        alt={title}
+        class="w-32 h-32 object-cover rounded-md shadow-md mx-auto mb-4"
+    />
+    <h1 class="text-2xl font-bold">{title}</h1>
+    <main class="max-w-screen-md prose sm:prose-lg md:prose-xl lg:prose-2xl xl:prose-3xl">
+        <p class="text-3xl">{shortDescription}</p>
+        <div class={isExpanded ? 'block' : 'hidden'}>
+            <p>{fullDescription}</p>
+        </div>
+        <button on:click={toggleDescription} class="text-blue-500 cursor-pointer">
+            {isExpanded ? 'Collapse' : 'Expand'} Description
+        </button>
+    </main>
+</div>
