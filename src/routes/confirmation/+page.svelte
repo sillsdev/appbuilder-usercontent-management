0ab28d7c075369514c81d81f1093fda7f5120967
Sharon Patta <sharon.patta@gordon.edu>
2024-03-10 23:07:28 -0400

    Add styling to confirmation page

--- src/routes/confirmation/+page.svelte
+++ src/routes/confirmation/+page.svelte
@@ -1,4 +1,27 @@
-<h1>
-    Thank you for requesting deletion of your data/account! A team member will be notified, and your
-    data/account will be deleted.
-</h1>
+<div role="alert" class="alert alert-success">
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="stroke-current shrink-0 h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+        /></svg
+    >
+    <h1>
+        Thank you for requesting deletion of your data/account! A team member has been notified, and
+        your data/account will be deleted soon.
+    </h1>
+</div>
+
+<style>
+    div {
+        padding-left: 160px;
+        padding-right: 160px;
+        position: absolute;
+        top: 50%;
+    }
+</style>
