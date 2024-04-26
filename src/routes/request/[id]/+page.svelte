1e99cb029855ccc476526843f573619a603d920d
Sharon Patta <sharon.patta@gordon.edu>
2024-04-26 17:40:23 -0400

    Remove unused style

--- src/routes/request/[id]/+page.svelte
+++ src/routes/request/[id]/+page.svelte
@@ -62,15 +62,6 @@
 </div>
 
 <style>
-    .center {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        padding: 10px;
-        text-align: center;
-    }
-
     div.question {
         display: flex;
         flex-direction: column;
