f3f8296781207901651048853a25220d0e9af0eb
Sharon Patta <sharon.patta@gordon.edu>
2024-04-29 11:39:41 -0400

    Progress on localization issue

--- src/routes/verify/[id]/+page.svelte
+++ src/routes/verify/[id]/+page.svelte
@@ -99,11 +99,7 @@
                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
             /></svg
         >
-        <span
-            >{$_('page.verify.spamFolder')}<span class="font-bold text-black"
-                >{PUBLIC_CONTACT_EMAIL}</span
-            >{$_('page.verify.assistance')}</span
-        >
+        <span>{$_('page.verify.assistance', { values: { email: { PUBLIC_CONTACT_EMAIL } } })}</span>
     </div>
 </div>
 
